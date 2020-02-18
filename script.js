2148309b09af3f80b7087b119389cbcf397f2fe4
{mwikiaBundi1} <{mwikiam@gmail.com}>
2020-02-18 20:52:03 +0300

    Add get gender function

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function getDay(name){
     var month = parseInt(document.getElementById("month").value);
     var date = parseInt(document.getElementById("date").value);
     var year = document.getElementById("year").value;
-    var century = year.slice(0, 2);
-    var years = year.slice(2, 4);
+    var century = year.slice(0, 1);
+    var years = year.slice(2, 3);
 
     if (month === "" || month < 1 || month > 12 || date === "" || date < 0 || date > 31 || year === "" || year.length > 4 || name === ""){
         alert("Confirm that you have entered the correct information!")
@@ -12,7 +12,16 @@ function getDay(name){
         return day
     }
 }
+function getGender(){
+    var radio = document.getElementsByName("gender");  
 
+    for (var i = 0; i < radio.length; i++){
+        if (radio[i].checked){
+            var gender = radio[i].value;
+            return gender;
+        }
+    }
+}
 
 function getName(){
     var name =  document.getElementById("name").value;
@@ -21,9 +30,9 @@ function getName(){
     var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
     if (getGender() === "male"){
-        alert(`Hey ${name}! if you were born in Ghana your name would be ${male[getDay(name)]}`);
+        document.getElementById("post").innerHTML=(`Hey ${name}! if you were born in Ghana your name would be ${male[getDay(name)]}`);
     }else if(getGender() === "female"){
-        alert(`Hey ${name}! if you were born in Ghana your name would be ${female[getDay(name)]}`);
+        document.getElementById("post").innerHTML=(`Hey ${name}! if you were born in Ghana your name would be ${female[getDay(name)]}`);
     }else{
         alert("Confirm that you have selected your gender")
     }
